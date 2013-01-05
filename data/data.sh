673ad1dc930ac8c04da1641bf1483e5cbcad3765
Beau Gunderson <beau@beaugunderson.com>
2013-01-05 12:36:06 -0800

    Add script to get SF data, library to read it

---
+++ data/data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir sf
+
+pushd sf
+
+wget --no-check-certificate https://204.68.210.15/food/SFBusinesses11282012.zip
+
+unzip SFBusinesses11282012.zip
+
+rm SFBusinesses11282012.zip
